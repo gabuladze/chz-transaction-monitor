d3b62e0810bb6cf86764bf6c5426461c8957924a
Giorgi Abuladze <itisabu@gmail.com>
2021-12-24 17:30:44 +0400

    Added config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+module.exports = {
+  NODE_ENV: process.env.NODE_ENV || 'dev',
+  APP_PORT: process.env.APP_PORT || 8000,
+  ETH_NODE: {
+    WS_PORT: process.env.WS_PORT,
+    WS_HOST: process.env.WS_HOST,
+    WS_PROVIDER_TIMEOUT: 3000,
+    WS_PROVIDER_RECONNECT: true,
+    WS_PROVIDER_RECONNECT_DELAY: 2000,
+    WS_PROVIDER_RECONNECT_MAX_ATTEMPTS: 3,
+    WS_PROVIDER_RECONNECT_ON_TIMEOUT: true
+  }
+}
