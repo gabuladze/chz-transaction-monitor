e606978462062fc2607711bf9a341eff356e35f8
Giorgi Abuladze <itisabu@gmail.com>
2021-12-26 16:47:43 +0400

    Close mongo connection after the tests finish

--- test/teardown.js
+++ test/teardown.js
@@ -1,7 +1,9 @@
 
 const Web3Singleton = require('../web3')
+const mongo = require('../db')
 
 after(async function () {
   const web3 = await Web3Singleton.getInstance()
   web3.currentProvider.disconnect()
+  await mongo.close()
 })
