9267e5bfb841975fca0429d2147e3c94cb7448a9
Giorgi Abuladze <itisabu@gmail.com>
2021-12-26 15:37:35 +0400

    Added test for TransactionsService.isTokenTransaction

--- test/transactionsService.test.js
+++ test/transactionsService.test.js
@@ -59,4 +59,30 @@ describe('Test TransactionsService', function () {
       }
     })
   })
+
+  describe('#isTokenTransaction', function () {
+    it('must return true for transaction that interacts directly with token contract', async function () {
+      const testTransactionHash = '0x0a388d3d8b5a5b9d622af074911e3bf88ed7249a3935d24c25d01ea66ea55986'
+
+      const result = await TransactionsServiceInstance.isTokenTransaction(testTransactionHash)
+
+      expect(result).to.equal(true)
+    })
+
+    it('must return true for internal contract transaction', async function () {
+      const testTransactionHash = '0x52c550eaf791bd69821dc42d2a17ade9c97cc0a660e45619865a69b11b02dac0'
+
+      const result = await TransactionsServiceInstance.isTokenTransaction(testTransactionHash)
+
+      expect(result).to.equal(true)
+    })
+
+    it('must return false for non contract transaction', async function () {
+      const testTransactionHash = '0x0181c3e08abf4814ce65f762c4fde475fa6bf79df467bd5f0afba881dc287715'
+
+      const result = await TransactionsServiceInstance.isTokenTransaction(testTransactionHash)
+
+      expect(result).to.equal(false)
+    })
+  })
 })
