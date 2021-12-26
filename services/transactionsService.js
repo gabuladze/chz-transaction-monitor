eab7d5d156e01606dcac162a175dd84f7fb24b9f
Giorgi Abuladze <itisabu@gmail.com>
2021-12-26 15:14:17 +0400

    Implemented TransactionsService.getTransfers

--- services/transactionsService.js
+++ services/transactionsService.js
@@ -1,7 +1,47 @@
+const Decimal = require('decimal.js')
 const Service = require('./service.js')
+const IERC20 = require('../abis/IERC20')
+const { TOKEN } = require('../config')
 
 class TransactionsService extends Service {
+  /**
+   * Returns transfer data from transactionReceipt.logs
+   * @async
+   * @param {object} transactionReceipt - Transaction receipt object
+   * @returns array containing transfer data extracted from transaction logs
+   */
+  async getTransfers (transactionReceipt) {
+    const Contract = new this.web3.eth.Contract(IERC20, TOKEN.CONTRACT_ADDRESS)
+    const decimals = await Contract.methods.decimals().call()
 
+    // Index 0 is always topic hash, indexes 1-3 contain indexed params of the log
+    const transferLogs = transactionReceipt.logs.filter((log) => log.address.toLowerCase() === TOKEN.CONTRACT_ADDRESS && log.topics[0] === TOKEN.TOPIC_HASH.Transfer)
+    if (transferLogs.length === 0) return []
+
+    const transfers = []
+    for (let i = 0; i < transferLogs.length; i++) {
+      const transferLog = transferLogs[i]
+
+      const from = this.web3.eth.abi.decodeParameter('address', transferLog.topics[1]).toLowerCase()
+      const to = this.web3.eth.abi.decodeParameter('address', transferLog.topics[2]).toLowerCase()
+      const tokenAmountNative = this.web3.eth.abi.decodeParameter('uint256', transferLog.data)
+      const tokenAmount = Decimal.div(tokenAmountNative, Decimal.pow(10, decimals)).toString()
+
+      const transfer = {
+        transactionHash: transactionReceipt.transactionHash,
+        from,
+        to,
+        amount: {
+          native: tokenAmountNative,
+          [TOKEN.SYMBOL.toLowerCase()]: tokenAmount
+        }
+      }
+
+      transfers.push(transfer)
+    }
+
+    return transfers
+  }
 }
 
 module.exports = TransactionsService
