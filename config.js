e7535b446dc40adce95b9d367041aa7115a377b5
Giorgi Abuladze <itisabu@gmail.com>
2021-12-26 15:14:02 +0400

    Added token contract data to config

--- config.js
+++ config.js
@@ -12,5 +12,15 @@ module.exports = {
     WS_PROVIDER_RECONNECT_DELAY: 2000,
     WS_PROVIDER_RECONNECT_MAX_ATTEMPTS: 3,
     WS_PROVIDER_RECONNECT_ON_TIMEOUT: true
+  },
+  TOKEN: {
+    SYMBOL: 'CHZ',
+    CONTRACT_ADDRESS: '0x3506424f91fd33084466f402d5d97f05f8e3b4af',
+    TOPICS: [
+      '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' // Transfer(address,address,uint256)
+    ],
+    TOPIC_HASH: {
+      Transfer: '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
+    }
   }
 }
