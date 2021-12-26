e57717a17e20957b91a18696c083d5e6160005b2
Giorgi Abuladze <itisabu@gmail.com>
2021-12-26 12:14:25 +0400

    Run processBlock script on startup

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('started')
+const processBlock = require('./scripts/processBlock.js')
+
+async function start () {
+  try {
+    console.log('Starting processBlock script...')
+    await processBlock.run()
+  } catch (error) {
+    console.log('An Error has occured!', error.stack)
+  }
+}
+
+start()
