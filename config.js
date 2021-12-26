c0d5213e1ac8716342283d7b9b9ba51b74d166e5
Giorgi Abuladze <itisabu@gmail.com>
2021-12-26 14:28:54 +0400

    Added INFURA_URL to config

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'dev',
   APP_PORT: process.env.APP_PORT || 8000,
   ETH_NODE: {
+    INFURA_URL: process.env.INFURA_URL,
     WS_PORT: process.env.WS_PORT,
     WS_HOST: process.env.WS_HOST,
     WS_PROVIDER_TIMEOUT: 3000,
