c1744fc30c78cec4b09b35a842bf0553ea8743bb
Giorgi Abuladze <itisabu@gmail.com>
2021-12-27 17:45:07 +0400

    Drop transactions collection

--- scripts/initDb.js
+++ scripts/initDb.js
@@ -3,13 +3,25 @@ const { MONGODB } = require('../config')
 const db = mongo.db(MONGODB.DB_NAME)
 
 /**
- * Resets processedBlocks collection
+ * Recreates processedBlocks collection
  */
-const run = async () => {
-  const collections = await db.collections({ filter: { name: 'processedBlocks' }, nameOnly: true })
+const resetProcessedBlocks = async () => {
+  const collections = await db.listCollections({ name: 'processedBlocks' }).toArray()
   if (collections.length > 0) await db.collection('processedBlocks').drop()
-  await mongo.db(MONGODB.DB_NAME).createCollection('processedBlocks', { capped: true, max: 10, size: 100000000 })
+  await db.createCollection('processedBlocks', { capped: true, max: 10, size: 100000000 })
+}
 
+/**
+ * Drops transactions collection
+ */
+const resetTransactions = async () => {
+  const collections = await db.listCollections({ name: 'transactions' }).toArray()
+  if (collections.length > 0) await db.collection('transactions').drop()
+}
+
+const run = async () => {
+  await resetProcessedBlocks()
+  await resetTransactions()
   return true
 }
 
