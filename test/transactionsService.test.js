091405d3c385b218aba37e57e9509cc398ad3f4f
Giorgi Abuladze <itisabu@gmail.com>
2021-12-27 16:48:19 +0400

    Added tests for TransactionsService.processTransaction

--- test/transactionsService.test.js
+++ test/transactionsService.test.js
@@ -4,14 +4,20 @@ const { expect } = chai
 const mongo = require('../db')
 const web3Singleton = require('../web3')
 const TransactionsService = require('../services/transactionsService')
+const { MONGODB } = require('../config')
 
 let TransactionsServiceInstance = null
 let web3 = null // web3 instance
+const transactionsToDelete = []
 
 describe('Test TransactionsService', function () {
   before(async function () {
     web3 = await web3Singleton.getInstance()
     TransactionsServiceInstance = new TransactionsService({ web3, mongo })
+  })
+
+  after(async function () {
+    await mongo.db(MONGODB.DB_NAME).collection('transactions').deleteMany({ transactionHash: { $in: transactionsToDelete } })
   })
 
   describe('#getTransfers', function () {
@@ -88,6 +94,51 @@ describe('Test TransactionsService', function () {
       const result = await TransactionsServiceInstance.isTokenTransaction(testTransactionHash)
 
       expect(result).to.equal(false)
+    })
+  })
+
+  describe('#processTransaction', function () {
+    const assert = (result, transactionDbRecord) => {
+      expect(result).to.equal(true)
+      expect(transactionDbRecord).to.have.property('totalTokensTransfered').that.is.an('object')
+      expect(transactionDbRecord.totalTokensTransfered).to.have.property('native').that.is.a('number')
+      expect(transactionDbRecord.totalTokensTransfered).to.have.property('chz').that.is.a('number')
+    }
+
+    it('must return true for transaction that interacts directly with token contract', async function () {
+      const testTransactionHash = '0x0a388d3d8b5a5b9d622af074911e3bf88ed7249a3935d24c25d01ea66ea55986'
+      const testTransactionReceipt = await web3.eth.getTransactionReceipt(testTransactionHash)
+
+      const result = await TransactionsServiceInstance.processTransaction(testTransactionReceipt)
+      const transactionDbRecord = await mongo.db(MONGODB.DB_NAME).collection('transactions')
+        .findOne({ transactionHash: testTransactionReceipt.transactionHash })
+      transactionsToDelete.push(testTransactionReceipt.transactionHash)
+
+      assert(result, transactionDbRecord)
+    })
+
+    it('must return true for internal contract transaction', async function () {
+      const testTransactionHash = '0x52c550eaf791bd69821dc42d2a17ade9c97cc0a660e45619865a69b11b02dac0'
+      const testTransactionReceipt = await web3.eth.getTransactionReceipt(testTransactionHash)
+
+      const result = await TransactionsServiceInstance.processTransaction(testTransactionReceipt)
+      const transactionDbRecord = await mongo.db(MONGODB.DB_NAME).collection('transactions')
+        .findOne({ transactionHash: testTransactionReceipt.transactionHash })
+      transactionsToDelete.push(testTransactionReceipt.transactionHash)
+
+      assert(result, transactionDbRecord)
+    })
+
+    it('must return true for non contract transaction. db must not have a record of the transaction', async function () {
+      const testTransactionHash = '0x0181c3e08abf4814ce65f762c4fde475fa6bf79df467bd5f0afba881dc287715'
+      const testTransactionReceipt = await web3.eth.getTransactionReceipt(testTransactionHash)
+
+      const result = await TransactionsServiceInstance.processTransaction(testTransactionReceipt)
+      const transactionDbRecord = await mongo.db(MONGODB.DB_NAME).collection('transactions')
+        .findOne({ transactionHash: testTransactionReceipt.transactionHash })
+
+      expect(result).to.equal(true)
+      expect(transactionDbRecord).to.equal(null)
     })
   })
 })
