80a738350334ddfd6e1bb4219067d83e43901cc4
Giorgi Abuladze <itisabu@gmail.com>
2021-12-28 15:47:34 +0400

    Added TransactionsService.processTransactions

--- services/transactionsService.js
+++ services/transactionsService.js
@@ -5,11 +5,34 @@ const { TOKEN, MONGODB } = require('../config')
 
 class TransactionsService extends Service {
   /**
+   * Retrieves transaction receipts for specified hashes
+   * and binds processTransactionReceiptResponse to each request response
+   * @async
+   * @param {string[]} transactionHashes - Array of transaction hashes
+   */
+  async processTransactions (transactionHashes) {
+    const batchRequest = new this.web3.BatchRequest()
+    console.log(`Creating batch request for ${transactionHashes.length} transactions`)
+
+    for (let i = 0; i < transactionHashes.length; i++) {
+      const hash = transactionHashes[i]
+      batchRequest.add(
+        this.web3.eth.getTransactionReceipt.request(hash, this.processTransactionReceiptResponse.bind(this))
+      )
+    }
+
+    await batchRequest.execute()
+  }
+
+  /**
    * Saves token transaction to db
    * @async
    * @param {Object} transactionReceipt - Transaction receipt
    */
-  async processTransaction (transactionReceipt) {
+  async processTransactionReceiptResponse (error, transactionReceipt) {
+    if (error) {
+      console.log('Error when fetching transaction receipt:', error.stack)
+    }
     const transfersResult = await this.getTransfers(transactionReceipt)
     if (transfersResult.transfers.length === 0) return true
 
@@ -18,7 +41,7 @@ class TransactionsService extends Service {
 
     // Save transaction to db
     await this.mongo.db(MONGODB.DB_NAME).collection('transactions').insertOne(transactionReceipt)
-
+    console.log('Saved:', transactionReceipt.transactionHash)
     return true
   }
 
