6138c50a38dddf2137da6d7a8fd45f07ca4e3ba7
Giorgi Abuladze <itisabu@gmail.com>
2021-12-29 17:00:57 +0400

    Run initDb script on startup, start http server

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+const mongo = require('./db')
 const processBlock = require('./scripts/processBlock.js')
+const initDb = require('./scripts/initDb.js')
+
+const { APP_PORT } = require('./config')
+const app = require('./app.js')
 
 async function start () {
   try {
+    await mongo.connect()
+    await initDb.run()
+
+    app.listen(APP_PORT, () => console.log(`chz-transaction-monitor listening on port ${APP_PORT}`))
+
     console.log('Starting processBlock script...')
     await processBlock.run()
   } catch (error) {
